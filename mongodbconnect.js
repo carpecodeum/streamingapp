a410d22ca5450337c124d804d9e100b8f14ca691
Aditya Bhatnagar <43586052+carpecodeum@users.noreply.github.com>
2019-06-22 17:31:39 +0530

    Delete mongodbconnect.js

--- mongodbconnect.js
+++
@@ -1,7 +0,0 @@
-var mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/userlogin', {useNewUrlParser: true});
-var db = mongoose.connection;
-db.on('error', console.error.bind(console, 'connection error:'));
-db.once('open', function() {
-  console.log("connected");
-});
